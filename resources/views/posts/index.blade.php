dad6128865cae57d2f194197aa0138601baf6519
BrettAkey <brett.akey@gmail.com>
2019-12-10 13:46:05 -0500

    template change

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
     @section('content')
         @foreach ($posts as $post)
-            <h1><a href="/post/{{ $post->id }}">{{ $post->title }}</a></h1>
+            <a href="/post/{{ $post->id }}">{{ $post->title }}</a>
             <p>Last upadted at: {{ $post->updated_at }}</p>
         @endforeach       
     @endsection
