81ba232cb17f9b3628d91b1acce6038836ec7767
BrettAkey <brett.akey@gmail.com>
2020-02-27 16:54:16 -0500

    url returns title instead of id

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,7 @@
     <p><a href="{{route('post.create')}}">CLICK HERE</a> to create a post.</p>
     <h2>Posts</h2>
         @foreach ($posts as $post)
-            <a href="{{ route('post.show', $post) }}">{{ $post->title }}</a>
+            <a href="{{ route('post.show', $post->title) }}">{{ $post->title }}</a>
             <p>Last upadted at: {{ $post->updated_at }}</p>
         @endforeach       
     @endsection
