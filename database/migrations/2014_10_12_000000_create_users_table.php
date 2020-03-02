5b64ecb312eae23fc3037c11ba2fa54819b3ce2f
BrettAkey <brett.akey@gmail.com>
2020-03-02 16:57:16 -0500

    added factories

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,15 +23,6 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-
-        $data = array(
-            array('id'=>'1', 'name'=>'Ms. Emilia Klein', 'email'=> 'edwin.herzog@example.com', 'email_verified_at'=> '2020-03-02 13:01:12',
-        'updated_at'=> '2020-03-02 13:01:12', 'created_at'=> '2020-03-01 19:45:33', 
-        'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'remember_token'=>'8Q8g4iD1Hx'),
-        array('id'=>'2', 'name'=>'Pat Grant', 'email'=> 'audie47@example.net', 'email_verified_at'=> '2020-03-01 14:01:12',
-        'updated_at'=> '2020-03-01 14:01:12', 'created_at'=> '2020-02-22 19:45:33', 
-        'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'remember_token'=>'wJrAL12yWs'));
-        User::insert($data);
     }
 
     /**
--- database/migrations/2019_12_03_205628_create_posts_table.php
+++ database/migrations/2019_12_03_205628_create_posts_table.php
@@ -23,20 +23,6 @@ class CreatePostsTable extends Migration
             $table->text('body');
             $table->timestamps();
         });
-
-        $data = array(
-            array('title'=>'Migration Structure', 'body'=>'A migration class contains two methods: up and down. The up method is used to add new tables, columns, 
-            or indexes to your database, while the down method should reverse the operations performed by the up method.', 
-            'created_at'=> '2019-12-5 16:50:20', 'updated_at'=> '2019-12-6 17:45:12', 'user_id'=>'1'),
-            array('title'=>'Running Migrations', 'body'=>'To run all of your outstanding migrations, execute the migrate Artisan command.',
-            'created_at'=> '2019-12-08 13:45:10', 'updated_at'=> '2019-12-08 13:45:10', 'user_id'=>'2'),
-            array('title'=>'Rolling Back Migrations', 'body'=>'To rollback the latest migration operation, you may use the rollback command. 
-            This command rolls back the last "batch" of migrations, which may include multiple migration files.',
-            'created_at'=> '2019-12-10 17:42:04', 'updated_at'=> '2019-12-10 17:42:04', 'user_id'=>'2'),
-            array('title'=>'Hello World', 'body'=>'Welcome to the greatest pratice ever.',
-            'created_at'=> '2020-02-20 17:42:04', 'updated_at'=> '2020-02-26 13:05:04', 'user_id'=>'1'),
-        );
-        Post::insert($data);
     }
 
     /**
